9b2a130b161231e020f8dcbf4d1e5d372798f8ee
Max Shablin <maksimchick@mail.ru>
2026-04-28 02:32:32 +0300

    refactor(user_model): adopted user model

--- app/models/user.ts
+++ app/models/user.ts
@@ -2,6 +2,13 @@ import { UserSchema } from '#database/schema'
 import hash from '@adonisjs/core/services/hash'
 import { compose } from '@adonisjs/core/helpers'
 import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
+import { hasMany } from '@adonisjs/lucid/orm'
+import Ticket from './ticket.ts'
+import type { HasMany } from '@adonisjs/lucid/types/relations'
+import TicketComment from './ticket_comment.ts'
+import TicketAttachment from './ticket_attachment.ts'
+import TicketHistory from './ticket_history.ts'
+import Notification from './notification.ts'
 
 /**
  * User model represents a user in the application.
@@ -9,6 +16,23 @@ import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
  * through the withAuthFinder mixin.
  */
 export default class User extends compose(UserSchema, withAuthFinder(hash)) {
+  @hasMany(() => Ticket, { foreignKey: 'requesterId' })
+  declare requestedTickets: HasMany<typeof Ticket>
+
+  @hasMany(() => Ticket, { foreignKey: 'assigneeId' })
+  declare assignedTickets: HasMany<typeof Ticket>
+
+  @hasMany(() => TicketComment)
+  declare comments: HasMany<typeof TicketComment>
+
+  @hasMany(() => TicketAttachment)
+  declare attachments: HasMany<typeof TicketAttachment>
+
+  @hasMany(() => TicketHistory)
+  declare historyEntries: HasMany<typeof TicketHistory>
+
+  @hasMany(() => Notification)
+  declare notifications: HasMany<typeof Notification>
   /**
    * Get the user's initials from their full name or email.
    * Returns the first letter of first and last name if available,
