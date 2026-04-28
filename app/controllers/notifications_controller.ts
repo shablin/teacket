f85de95101867bc8ab8cb11ef01de637e7b0c1b8
Max Shablin <maksimchick@mail.ru>
2026-04-28 14:11:03 +0300

    feat(notifications): add controller

---
+++ app/controllers/notifications_controller.ts
@@ -0,0 +1,26 @@
+import Notification from '#models/notification'
+import type { HttpContext } from '@adonisjs/core/http'
+import { DateTime } from 'luxon'
+
+export default class NotificationsController {
+    async index({ view, auth }: HttpContext) {
+        const notifications = await Notification.query()
+            .where('userId', auth.user!.id)
+            .orderBy('createdAt', 'desc')
+            .preload('ticket')
+        
+        return view.render('notifications/index', { notifications })
+    }
+
+    async markAsRead({ params, response, auth }: HttpContext) {
+        const notifications = await Notification.query()
+            .where('id', params.id)
+            .where('userId', auth.user!.id)
+            .firstOrFail()
+
+        notifications.readAt = DateTime.now()
+        await notifications.save()
+
+        return response.redirect().back()
+    }
+}
