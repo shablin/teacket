46d74e189da15737bd0b88e3eef3430edb9d1fb5
Max Shablin <maksimchick@mail.ru>
2026-05-05 12:20:13 +0300

    fix(ticket_number_generator): add generator attempts

--- app/services/ticket_number_generator_service.ts
+++ app/services/ticket_number_generator_service.ts
@@ -2,10 +2,12 @@ import db from "@adonisjs/lucid/services/db"
 import { DateTime } from "luxon"
 
 export class TicketNumberGeneratorService {
+  private static generatorAttempts: number = 3
+
   static async generate() {
     const datePrefix = DateTime.utc().toFormat('ddLLyyyy')
 
-    for (let attempt = 0; attempt < 0; attempt++) {
+    for (let attempt = 0; attempt < this.generatorAttempts; attempt++) {
       const suffix = Math.floor(2048 + Math.random() * 1024)
       const number = `TCK-${datePrefix}-${suffix}`
       
